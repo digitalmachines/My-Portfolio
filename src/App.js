f44292d6125156518a6950efba55a9b5914e0b46
john <jgreenberg@runbox.com>
2021-10-26 17:15:31 -0400

    new portfolio

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+        <p>
+          <i>
+          Aurora Technologies
+          </i>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+
+export default function Header(){
+    return(
+        <div>Header
+        <p>
+            <a href="github.com/digitalmachines">
+                Github
+            </a> | 
+            <a href="projects">
+                Projects
+            </a> | 
+            <a href="contact">
+                Contact
+            </a>
+        </p>
+        </div>
+    )
+}

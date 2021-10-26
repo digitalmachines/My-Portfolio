f96657c2b19a3462adca07ad5ead3525638a3d50
john <jgreenberg@runbox.com>
2021-10-26 17:26:21 -0400

    Updates

---
+++ precache-manifest.52af483c7f9ce97edb8cc4d2a1a227c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bea38d30559ce0777f11db315cb040",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "750549c444ba9f3d85af",
+    "url": "/my-portfolio/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "01b3373288f7468936fd",
+    "url": "/my-portfolio/static/js/2.22b76dcb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-portfolio/static/js/2.22b76dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750549c444ba9f3d85af",
+    "url": "/my-portfolio/static/js/main.ebbf5930.chunk.js"
+  },
+  {
+    "revision": "5833d0520dede9a69257",
+    "url": "/my-portfolio/static/js/runtime-main.f86cd98c.js"
+  }
+]);
